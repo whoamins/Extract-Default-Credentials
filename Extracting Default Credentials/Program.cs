790aaa410d635e62d1c8a4b8724f486cc5e73225
whoamins <40am1ns@protonmail.com>
2022-02-04 21:32:10 +0300

    init

---
+++ Extracting Default Credentials/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+
+const string regKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
+
+if (OperatingSystem.IsWindows())
+{
+    using var winLogonRegistryKey = Registry.LocalMachine.OpenSubKey(regKey);
+
+    if (winLogonRegistryKey != null)
+    {
+        Console.WriteLine($"Default Domain Name: {winLogonRegistryKey.GetValue("DefaultDomainName")}");
+        Console.WriteLine($"Default User Name: {winLogonRegistryKey.GetValue("DefaultUserName")}");
+        Console.WriteLine($"Default Password: {winLogonRegistryKey.GetValue("DefaultPassword")}");
+        Console.WriteLine($"AutoAdminLogon: {winLogonRegistryKey.GetValue("AutoAdminLogon")}");
+    }
+}
+else
+{
+    Console.WriteLine("It doesn't work on Linux :)");
+}
